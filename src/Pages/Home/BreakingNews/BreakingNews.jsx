e6061b60bbaece04221130c426d21410bbe619b6
Chowdhury Nazmuz Sakib <nazmuz.sakib@brainstation-23.com>
2024-05-24 15:58:22 +0600

    header and breaking news section added

---
+++ src/Pages/Home/BreakingNews/BreakingNews.jsx
@@ -0,0 +1,26 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
+const BreakingNews = () => {
+  return (
+    <div className="flex bg-slate-100">
+      <button className="btn btn-secondary">Latest</button>
+      <Marquee pauseOnHover="true">
+        <Link className="mr-12" to="/">
+          I can be a React component, multiple React components, or just some
+          text.
+        </Link>
+        <Link className="mr-12" to="/">
+          I can be a React component, multiple React components, or just some
+          text.
+        </Link>
+        <Link className="mr-12" to="/">
+          I can be a React component, multiple React components, or just some
+          text.
+        </Link>
+      </Marquee>
+    </div>
+  );
+};
+
+export default BreakingNews;
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,7 +1,11 @@
+import logo from "../../../assets/logo.png";
+import moment from "moment";
 const Header = () => {
   return (
-    <div>
-      <h1 className="text3xl">Header</h1>
+    <div className="text-center">
+      <img className="mx-auto m-5" src={logo} alt=""></img>
+      <p>Journalism Without Fear or Favour</p>
+      <p className="m-3">{moment().format("dddd, MMMM D, YYYY")}</p>
     </div>
   );
 };
