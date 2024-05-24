2d9384565a1f838c3b293595592b306bc1e0d5b0
Chowdhury Nazmuz Sakib <nazmuz.sakib@brainstation-23.com>
2024-05-24 12:07:54 +0600

    .env file updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBS1D8LZmwVfhfw-A0cvF9laDJxKUWZ5xo",
-  authDomain: "dragon-news-da954.firebaseapp.com",
-  projectId: "dragon-news-da954",
-  storageBucket: "dragon-news-da954.appspot.com",
-  messagingSenderId: "120807052728",
-  appId: "1:120807052728:web:0869eb66926a0e200a6b4d",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
