f09c547ce4e5f0d6651d8bc380be4bc91c8788af
Chowdhury Nazmuz Sakib <nazmuz.sakib@brainstation-23.com>
2024-05-24 11:23:38 +0600

    project setup done with firebase,router, tailwind css,daisy UI

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBS1D8LZmwVfhfw-A0cvF9laDJxKUWZ5xo",
+  authDomain: "dragon-news-da954.firebaseapp.com",
+  projectId: "dragon-news-da954",
+  storageBucket: "dragon-news-da954.appspot.com",
+  messagingSenderId: "120807052728",
+  appId: "1:120807052728:web:0869eb66926a0e200a6b4d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
